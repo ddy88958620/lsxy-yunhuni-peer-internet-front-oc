bf3e89c9fce14f8eb832158e96be9a8a85d22f69
Lluvio <caotousir@gmail.com>
2016-07-08 11:10:15 +0800

    add login cookie

---
+++ app/utils/cookieAuth.js
@@ -0,0 +1,17 @@
+import Cookie from 'js-cookie'
+import domain from '../config/domain.js' 
+
+console.log(domain);
+const cookieConfig =  {domain: domain.COOKIE_DOMAIN, expires: 3}
+
+export function saveCookie(key ,value) {
+  Cookie.set(key, value, cookieConfig)
+}
+
+export function removeCookie(key) {
+  Cookie.remove(key, cookieConfig)
+}
+
+export function getCookie(key) {
+  return Cookie.get(key, cookieConfig)
+}
--- app/vuex/modules/auth.js
+++ app/vuex/modules/auth.js
@@ -1,10 +1,6 @@
 import {
-  LOGIN_SUCCESS,
-  USERINFO_SUCCESS,
-  USERINFO_FAILURE,
-  LOGOUT_USER,
-  UPDATE_USESR_SUCCESS
-} from '../types'
+  LOCAL_LOGIN,
+} from '../mutation-types.js'
 
 const state = {
   token: '',
@@ -12,22 +8,9 @@ const state = {
 }
 
 const mutations = {
-  [LOGIN_SUCCESS](state, action){
-    state.token = action.token
-  },
-  [USERINFO_SUCCESS](state, action) {
-    state.user = action.user
-  },
-  [USERINFO_FAILURE](state, action) {
-    state.user = null
-  },
-  [LOGOUT_USER](state, action) {
-    state.token = null
-    state.user = null
-  },
-  [UPDATE_USESR_SUCCESS](state, action) {
-    state.user = action.user
-  },
+  [LOCAL_LOGIN](state, user){
+    state.user = {...user}
+  }
 }
 
 export default {
