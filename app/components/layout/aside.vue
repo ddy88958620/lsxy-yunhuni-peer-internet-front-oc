595e5b09cc56dcd9a7254f2c2b1aae590875a7f1
chenjj <chenjj@yunhuni.com>
2016-07-16 15:59:08 +0800

    头部样式,左边菜单栏样式

--- app/components/layout/aside.vue
+++ app/components/layout/aside.vue
@@ -3,39 +3,40 @@
 .app-sidebar {
   width: $sidebar-width;
   background-color: #616888;
-
   a {
     color: white;
   }
-
+  .active{
+    border-left: 3px solid #f6f883;
+    background-color: #515a76;
+    color: #fff;
+  }
   ul {
     list-style: none;
     padding: 0;
-    $height: 50px;
-
+    margin: 0;
     li {
       a {
         width: 100%;
-        padding-left: 15px;
-        height: $height;
-        line-height: $height;
-        display: inline-block;
+        padding: 15px;
+        &:hover{
+          padding-left: 12px;
+          @extend .active;
+        }
       }
       a.v-link-active {
         @extend a;
-        border-left: 3px solid #f6f883;
-        background-color: #515a76;
-        color: #fff;
+        @extend .active;
+        padding-left: 12px;
       }
     }
   }
-
 }
 </style>
 <template>
   <aside class='app-sidebar'>
-    <ul>
-      <li v-for='menu in menus'>
+    <ul class="flex-direction-column-reverse fex fex-1 ">
+      <li class="flex" v-for='menu in menus'>
         <a v-link="menu.link">{{ menu.title }}</a>
       </li>
     </ul>
