7ac7c526c1a39bab7701f63a3b2a00512806c27c
Lluvio <caotousir@gmail.com>
2016-07-19 15:23:55 +0800

    add .

---
+++ app/components/pages/dashboard/section-one/pie.vue
@@ -0,0 +1,39 @@
+<style scoped>
+span {
+
+}
+</style>
+<template>
+  <input
+    data-angleOffset=90
+    data-linecap=round
+    data-thickness="0.1"
+    type="text"
+    class="dial"
+    data-fgColor='#89d9e3'
+    data-width="80"
+    data-height="80"
+    data-min="0"
+    data-max="2000"
+    value="1534"/>
+</template>
+
+<script>
+require('jquery-knob')
+
+export default {
+  computed: {
+    angleOffset: {
+      handle(){
+        return
+      }
+    }
+  },
+  ready(){
+    console.log('ready')
+      $(".dial").knob({
+        readOnly: true
+      });
+  }
+}
+</script>
