0aa33c592283c35826d55e84c42f484aea4ee7e7
Lluvio <caotousir@gmail.com>
2016-07-20 11:53:53 +0800

    add all chart 2

---
+++ app/components/pages/dashboard/section-six/index.vue
@@ -0,0 +1,20 @@
+<template>
+    <!-- 区负荷量 -->
+    <section class='section_six'>
+      <chart :uuid="'realtime-area-chart'" :label="['1区','2区','3区']"></chart>
+    </section>
+</template>
+<script>
+export default {
+  components: {
+    'chart': require('../../../ui/realtime-chart.vue'),
+  }
+}
+</script>
+
+<style>
+
+.section_six {
+  background-color: white;
+}
+</style>
