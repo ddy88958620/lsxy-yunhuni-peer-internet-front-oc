033a19b0668ef77e2f1b3768ae107bed8f856e47
Lluvio <caotousir@gmail.com>
2016-08-03 15:49:28 +0800

    add node middleware

---
+++ middleware/app.js
@@ -0,0 +1,47 @@
+const Koa = require('koa');
+const app = new Koa();
+const router = require('koa-router')();
+const views = require('koa-views');
+const co = require('co');
+const convert = require('koa-convert');
+const json = require('koa-json');
+const onerror = require('koa-onerror');
+const bodyparser = require('koa-bodyparser')();
+const logger = require('koa-logger');
+
+
+const index = require('./routes/index');
+
+// middlewares
+app.use(convert(bodyparser));
+app.use(convert(json()));
+app.use(convert(logger()));
+app.use(convert(require('koa-static')(__dirname + '/public')));
+
+app.use(views(__dirname + '/views', {
+  extension: 'jade'
+}));
+
+// app.use(views(__dirname + '/views-ejs', {
+//   extension: 'ejs'
+// }));
+
+
+// logger
+app.use(async (ctx, next) => {
+  const start = new Date();
+  await next();
+  const ms = new Date() - start;
+  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`);
+});
+
+app.use(index.routes(), index.allowedMethods());
+// response
+
+app.on('error', function(err, ctx){
+  console.log(err)
+  log.error('server error', err, ctx);
+});
+
+
+module.exports = app;
