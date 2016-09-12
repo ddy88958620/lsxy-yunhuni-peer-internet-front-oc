79866bb20b6343156519fc42813f2a2ca811503b
chenjj <chenjj@yunhuni.com>
2016-09-12 10:27:25 +0800

    更新

--- middleware/config.js
+++ middleware/config.js
@@ -5,36 +5,36 @@ console.log(env)
 if(env === 'production') {
   // 生产
 	module.exports = {
-		COOKIEDOAIM: 'oc.dev.yunhuni.com',
+		COOKIEDOAIM: 'oc.yunhuni.cn',
 		COOKIENAME: 'YUNHUNISESSIONID',
-		JAVAAPI: 'http://10.171.69.136:18082',
-		REDISHOST: '10.162.199.238',
+		JAVAAPI: 'http://rest.oc.internal.yunhuni.cn',
+		REDISHOST: '10.163.3.165',
 		REDISPORT: 6379
 	}
 }
 else if (env === 'test') {
 	module.exports = {
 		// 测试
-		COOKIEDOAIM: 'localhost',
+		COOKIEDOAIM: 'oc.yunhuni.cn',
 		COOKIENAME: 'YUNHUNISESSIONID',
-		JAVAAPI: 'http://192.168.10.201:18082',
-		REDISHOST: '127.0.0.1',
+		JAVAAPI: 'http://rest.oc.internal.yunhuni.cn',
+		REDISHOST: '10.163.3.165',
 		REDISPORT: 6379
 	}
 }
 else if (env === 'develop') {
 	module.exports = {
   // 开发
-		COOKIEDOAIM: 'localhost',
+		COOKIEDOAIM: 'oc.dev.yunhuni.com',
 		COOKIENAME: 'YUNHUNISESSIONID',
 		JAVAAPI: 'http://192.168.10.201:18082',
 		REDISHOST: '127.0.0.1',
 		REDISPORT: 6379
 	}
 }
-else if(env === 'local') {
-	//本地
+else if (env === 'local') {
 	module.exports = {
+  // 本地
 		COOKIEDOAIM: 'localhost',
 		COOKIENAME: 'YUNHUNISESSIONID',
 		JAVAAPI: 'http://192.168.10.75:18082',
