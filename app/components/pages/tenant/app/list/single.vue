e6ad44d5a7c86e81f77d0d1ac8954487a1935b90
chenjj <chenjj@yunhuni.com>
2016-09-26 10:17:52 +0800

    修改租户中心样式,修改文案

--- app/components/pages/tenant/app/list/single.vue
+++ app/components/pages/tenant/app/list/single.vue
@@ -7,7 +7,7 @@
 			<div class="flex flex-1 flex-direction-column justify-content-c align-items-c">
 				<img src="../../../../../assets/images/app.png" width="100" height="100">
 				<p class="bigger-font-size name">{{ app.name}}</p>
-				<p>APPID: {{ app.id}}</p>
+				<p>应用标识: {{ app.id}}</p>
 				<p class="text-center">{{ app.description ? app.description :'这家伙很懒，什么都没留下'}}</p>
 			</div>
 		</header>
