3528ed2d6ff213d2d61ce72a5310cd0fcd7597e0
liuws@yunhuni.com <liuws@yunhuni.com>
2016-09-26 11:56:53 +0800

    请求拦截器，解决session中间件只会在内容改变的时候更新过期时间的bug

--- middleware/routes/index.js
+++ middleware/routes/index.js
@@ -8,8 +8,8 @@ const fs = require('fs')
 const config = require('../config')
 
 //请求拦截器，解决session中间件只会在内容改变的时候更新过期时间的bug
-router.use(async (ctx, next) => {
-	ctx.session.counter = ctx.session.refresh?false:true;
+	router.use(async (ctx, next) => {
+	ctx.session.refresh = ctx.session.refresh?false:true;
 });
 
 // 下面代码还需重构
