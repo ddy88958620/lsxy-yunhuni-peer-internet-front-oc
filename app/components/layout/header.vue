092bf189e79ba53c7fd802467275dad2532b1eb4
chenjj <chenjj@yunhuni.com>
2016-10-18 11:15:25 +0800

    头部未处理事项

--- app/components/layout/header.vue
+++ app/components/layout/header.vue
@@ -23,8 +23,38 @@
       padding-right: 15px;
       padding-top: 15px;
     }
+    .matter{
+      height: 45px;
+       &:hover {
+        .tipbar{
+          display: block;
+        }
+       }
+    }
+
+    .tipbar{
+      position: absolute;
+      left: 185px;
+      display: none;
+      top:50px;
+      min-width: 300px;
+      #tringle-up{
+        width: 0;
+        height: 0;
+        position: absolute;
+        left:50%;
+        margin-left: -5px;
+        top:-5px;
+        border-left: 10px solid transparent;
+        border-right: 10px solid transparent;
+        border-bottom: 5px solid #dbe2ee;
+      }
+
+    }
     .topbar-btn{
-      padding: 15px 10px;
+      padding: 0px 10px;
+      height: 45px;
+      line-height: 30px;
     }
   }
 </style>
@@ -32,7 +62,36 @@
 <template>
   <header class='app-header '>
     <div class="nav-left  overflow">
-      <span class="topbar-btn" >欢迎您来到云呼你运营中心<!-- ， 您有&nbsp;<a href="#">3</a>&nbsp;件事项未处理</span><i class="iconfont icon-oc-handle"></i> -->
+      <div class="topbar-btn inline-block" >欢迎您来到云呼你运营中心 ，<div class="matter inline-block">您有&nbsp;3&nbsp;件事项未处理&nbsp;&nbsp;<i class="iconfont icon-oc-handle"></i>
+        <div class="tipbar">
+          <div id="tringle-up">
+
+          </div>
+
+          <div class="admin-table">
+            <table class="table">
+              <thead>
+              <tr>
+                <th>序号</th>
+                <th>事件</th>
+              </tr>
+              </thead>
+              <tbody>
+              <tr>
+                <td>1</td> <td>您有3个租户等待身份认证</td>
+              </tr>
+              <tr>
+                <td>2</td> <td>您有3个放音文件等待审核</td>
+              </tr>
+              <tr>
+                <td>3</td> <td>您有3个放音文件等待审核</td>
+              </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+      </div>
     </div>
     <div class="nav-right">
       <search
