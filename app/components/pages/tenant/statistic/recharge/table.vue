62f7539f1e305a8ccf91f4027883941a04128bcd
chenjj <chenjj@yunhuni.com>
2016-10-18 14:39:07 +0800

    充值明细

--- app/components/pages/tenant/statistic/recharge/table.vue
+++ app/components/pages/tenant/statistic/recharge/table.vue
@@ -1,5 +1,27 @@
 <template>
   <div>
+
+    <div class="flex flex-direction-column admin-table-header">
+      <div class="flex align-items-c remove-margin-bottom">
+        <span class='datetime-picker-label clear-padding-left'>充值渠道:</span>
+        <select class="form-control" v-model='type'>
+          <option value=''>全部</option>
+          <option value='UNIONPAY'>银联</option>
+          <option value='ALIPAY'>支付宝</option>
+          <option value='RENGONG'>运营充值</option>
+        </select>
+        <span class='datetime-picker-label'  v-if="type=='RENGONG' || type==''">充值类型: </span>
+        <select class="form-control" v-model='source' v-if="type=='RENGONG' || type==''">
+          <option value=''>全部</option>
+          <option value='MANUAL_ACTIVITY' >手工-活动赠送</option>
+          <option value='MANUAL_BUSINESS'>手工-线下商务</option>
+          <option vaule='MANUAL_OTHER' >手工-其他</option>
+          <option vaule='MANUAL_TEST' >手工-测试</option>
+        </select>
+        <a class="btn btn-primary admin-button-margin" @click="query()" >查询</a>
+      </div>
+    </div>
+
     <div class="admin-table">
       <div class="table-total flex flex-1 justify-content-e float-r">
         共<span class="text-danger">{{recharge.totalCount}}</span>条
@@ -9,7 +31,7 @@
         <tr>
           <th class="text-align-c">充值时间</th>
           <th>充值额（元）</th>
-          <th>充值方式</th>
+          <th>充值渠道</th>
           <th>充值类型</th>
         </tr>
         </thead>
@@ -17,14 +39,17 @@
         <tr v-for='message in rechargeList'>
           <td class="message-time text-align-c">{{message.createTime | totalDate}}</td>
           <td>{{message.amount}}</td>
-          <td v-if="message.type==='RENGONG'">人工充值</td>
-          <td v-if="message.type==='UNIONPAY'">银联</td>
-          <td v-if="message.type==='ALIPAY'">支付宝</td>
-          <td v-if="message.source==='USER'">用户充值</td>
-          <td v-if="message.source==='MANUAL_ACTIVITY'">活动赠送</td>
-          <td v-if="message.source==='MANUAL_BUSINESS'">线下商务</td>
-          <td v-if="message.source==='MANUAL_OTHER'">其他</td>
-          <td v-if="message.source==='MANUAL_TEST'">测试</td>
+          <td>
+            <span v-if="message.type==='UNIONPAY' || message.type==='ALIPAY'">自助充值</span>
+            <span v-else>人工充值</span>
+          </td>
+          <td>
+            <span v-if="message.source==='USER'">用户自充</span>
+            <span v-if="message.source==='MANUAL_ACTIVITY'">手工-活动赠送</span>
+            <span v-if="message.source==='MANUAL_BUSINESS'">手工-线下商务</span>
+            <span v-if="message.source==='MANUAL_OTHER'">手工-其他</span>
+            <span v-if="message.source==='MANUAL_TEST'">手工-测试</span>
+          </td>
         </tr>
         </tbody>
       </table>
@@ -43,12 +68,13 @@
         recharge:{},
         rechargeTotal : 0,
         rechargeList: [],
-       
+        source:'',
+        type:''
       }
     },
     methods: {
       query(more){
-        //1.语音呼叫2.双向回拨3.会议服务4.IVR定制服务5.语音验证码6.录音服务
+
         let uid = this.$route.params.uid
         let type = 3
         let appId = this.$route.params.aid
